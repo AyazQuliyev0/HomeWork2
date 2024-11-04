bf2f99ae84c5ebbd29510cc6e41691eddc432529
Ayaz Quliyev <ayazaraz950@gmail.com>
2024-11-04 18:39:11 +0400

    sum of digits

---
+++ app.js
@@ -0,0 +1,8 @@
+let number = prompt("Please enter a number");
+
+let digit1 = parseInt(number.toString()[0]);
+let digit2 = parseInt(number.toString()[1]);
+let digit3 = parseInt(number.toString()[2]);
+let digit4 = parseInt(number.toString()[3]);
+let sum = digit1 + digit2 + digit3 + digit4;
+console.log(sum);
